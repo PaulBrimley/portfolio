bc97e24d96b6ce0a4da1c3325834ae5c8099a413
PaulBrimley <paul.k.brimley@gmail.com>
2016-10-21 18:41:30 -0600

    styling updates for carousel

--- core/app/components/projectMedia2.jsx
+++ core/app/components/projectMedia2.jsx
@@ -30,7 +30,7 @@ class ProjectMedia extends Component {
 
     getTransform() {
         if (this.props.index === this.props.activeSlide) {
-            return 'rotateY( ' + (this.props.projectMediaRotation * this.props.index) + 'deg ) translateX( -80% ) scale(2, 2)';
+            return 'rotateY( ' + (this.props.projectMediaRotation * this.props.index) + 'deg ) translateX( -85% ) scale(2, 2)';
         } else if (this.props.projectMediaArrayLength > 1) {
             return 'rotateY( ' + (this.props.projectMediaRotation * this.props.index) + 'deg ) translateX( -20px )';
         }
